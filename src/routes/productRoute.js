88eca832612332db02f1110d28b0feb6f852216f
Cr-Israel <carlosisrael08@hotmail.com>
2023-08-09 15:41:31 -0300

    feat: routes file

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -3,12 +3,14 @@ import { createProduct } from '../controllers/create.js'
 import { listAllProducts } from '../controllers/listAll.js'
 import { findById } from '../controllers/findById.js'
 import { update } from '../controllers/update.js'
+import { remove } from '../controllers/remove.js'
 
 const router = expressPromiseRouter()
 
 router.post('/products', createProduct)
 router.get('/products', listAllProducts)
 router.get('/products/:id', findById)
-router.put('/products/update/:id', update)
+router.put('/products/:id', update)
+router.delete('/products/:id', remove)
 
 export default router
